0b19baf6a1cbee89dd0f66af6a97853305dcca1e
ejfisc <ejfisc@protonmail.com>
2022-03-12 15:25:29 -0600

    added menu screen and title font

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ WIDTH, HEIGHT = 600, 800
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('Space Invaders')
 
+TITLE_FONT = pygame.font.Font(str(Path('Assets', 'space_invaders.ttf')), 50)
+
 PLAYER_WIDTH, PLAYER_HEIGHT = 50, 25
 BULLET_WIDTH, BULLET_HEIGHT = 5, 10
 
@@ -18,7 +20,31 @@ FPS = 60
 VEL = 4
 BULLET_VEL = 7
 
-def draw_window(bullets, player):
+TOP_SCORES = []
+
+# HOME SCREEN
+
+def draw_menu():
+    WIN.fill(BLACK)
+    title = TITLE_FONT.render('SPACE INVADERS', 1, WHITE)
+    WIN.blit(title, (WIDTH//2 - title.get_width()//2, 50))
+    pygame.display.update()
+
+def menu():
+    menu_state = True
+    while menu_state:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+            
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_RETURN:
+                game()
+
+        draw_menu()
+
+# GAME
+
+def draw_game(bullets, player):
     WIN.fill(BLACK)
     pygame.draw.rect(WIN, WHITE, player)
     for bullet in bullets:
@@ -30,37 +56,34 @@ def handle_player_movement(keys_pressed, player):
         player.x -= VEL
     if keys_pressed[pygame.K_RIGHT] and player.x + VEL + player.width < WIDTH - 5: # right
         player.x += VEL
-    # if keys_pressed[pygame.K_UP] and player.y - VEL > 0: # up
-    #   player.y -= VEL
-    # if keys_pressed[pygame.K_DOWN] and player.y + VEL + player.height < HEIGHT - 12: # down
-    #   player.y += VEL
 
-def handle_bullets(bullets, player):
-    for bullet in bullets:
+def handle_bullets(player_bullets, player):
+    for bullet in player_bullets:
         bullet.y -= BULLET_VEL
         if bullet.y <= -10:
-            bullets.remove(bullet)
+            player_bullets.remove(bullet)
 
-def main():
+def game():
     player = pygame.Rect(WIDTH//2 - (PLAYER_WIDTH//2), HEIGHT - (PLAYER_HEIGHT + 5), PLAYER_WIDTH, PLAYER_HEIGHT)
-    bullets = []
+    player_bullets = []
+    score = 0
     clock = pygame.time.Clock()
-    run = True
-    while run:
+    game_state = True
+    while game_state:
         clock.tick(FPS)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                run = False
+                game_state = False
                 pygame.quit()
 
             if event.type == pygame.KEYDOWN and event.key == pygame.K_UP:
                 bullet = pygame.Rect(player.x + PLAYER_WIDTH//2, player.y, BULLET_WIDTH, BULLET_HEIGHT)
-                bullets.append(bullet)
+                player_bullets.append(bullet)
 
         keys_pressed = pygame.key.get_pressed()
         handle_player_movement(keys_pressed, player)
-        handle_bullets(bullets, player)
-        draw_window(bullets, player)
+        handle_bullets(player_bullets, player)
+        draw_game(player_bullets, player)
 
 if __name__ == '__main__':
-    main()
+    menu()
