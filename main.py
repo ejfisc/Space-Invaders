1cd9da1957b6f060e63568a5c5884b1f1d01d2bd
ejfisc <ejfisc@protonmail.com>
2022-03-13 01:37:21 -0600

    removed aliens collided with bullets, added some todo lists for future tasks

--- main.py
+++ main.py
@@ -36,18 +36,27 @@ BULLET_VEL = 7
 ALIEN_VEL = 1
 
 # SCORE ARRAY
+# TODO: write scores to a file, read them to a list, and sort them
 top_scores = []
 
 # CLOCK
 clock = pygame.time.Clock()
 
 # HOME SCREEN
+
+# TODO:
+# draw actual menu
+# add background
+# show controls
 def draw_menu():
     WIN.fill(BLACK)
     title = TITLE_FONT.render('SPACE INVADERS', 1, WHITE)
     WIN.blit(title, (WIDTH//2 - title.get_width()//2, 50))
     pygame.display.update()
 
+# TODO:
+# select difficulty?
+# button to start game
 def menu(): 
     pygame.mixer.music.load(str(Path('Assets', 'menu.wav')))
     pygame.mixer.music.play(-1)
@@ -67,6 +76,13 @@ def menu():
         draw_menu()
 
 # GAME
+
+# TODO:
+# draw alien bullets
+# draw health
+# draw score
+# add alien images
+# draw hit combo score
 def draw_game(bullets, player, score, keys_pressed, aliens):
     WIN.fill(BLACK)
     WIN.blit(PLAYER, (player.x, player.y))
@@ -85,24 +101,38 @@ def player_movement(keys_pressed, player):
     if keys_pressed[pygame.K_RIGHT] and player.x + VEL + player.width < WIDTH - 5: # right
         player.x += VEL
 
+# TODO:
+# Fix it so the aliens don't collide with eachother
+# remove alien when they collide with player shield
+# decrease health when alien collides with player
 def alien_movement(aliens, player):
     for alien in aliens:
         if player.x > alien.x: # alien is to the left of player
            alien.x += ALIEN_VEL
         if player.x < alien.x:
            alien.x -= ALIEN_VEL
-        if player.y - ALIEN_HEIGHT > alien.y:
-            alien.y += ALIEN_VEL
+        alien.y += ALIEN_VEL
         if alien.colliderect(player):
             aliens.remove(alien)
         
-
-def handle_bullets(player_bullets, player):
+# TODO: 
+# Add bullet hit sound
+# Add alien bullets
+# Decrease life when alien bullet hits player
+# Remove alien bullet when bullet hits player shield
+# increment score when player shoots the bonus coin
+def handle_bullets(player_bullets, player, aliens):
     for bullet in player_bullets:
         bullet.y -= BULLET_VEL
         if bullet.y <= -10:
             player_bullets.remove(bullet)
+        for alien in aliens:
+            if alien.colliderect(bullet):
+                player_bullets.remove(bullet)
+                aliens.remove(alien)
 
+# TODO:
+# repopulate aliens after amount of time or alien deaths
 def populate_aliens():
     aliens = []
     for i in range(0, 5):
@@ -112,6 +142,11 @@ def populate_aliens():
         aliens.append(alien)
     return aliens
 
+# TODO:
+# add a bonus coin
+# increment score when player kills an alien
+# start combo when player kills an alien, and increment combo for each contiguous kill until the player misses
+# save top scores
 def game():
     pygame.mixer.music.load(str(Path('Assets', 'game.wav')))
     pygame.mixer.music.set_volume(0.1)
@@ -140,7 +175,7 @@ def game():
         
         player_movement(keys_pressed, player)
         alien_movement(aliens, player)
-        handle_bullets(player_bullets, player)
+        handle_bullets(player_bullets, player, aliens)
         draw_game(player_bullets, player, score, keys_pressed, aliens)
 
 if __name__ == '__main__':
